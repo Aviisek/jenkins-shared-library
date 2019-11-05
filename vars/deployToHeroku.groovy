b7391a44389df509d4b25c294efde3947e79a296
Abhishek K <abhishek.kumar.jec@gmail.com>
2019-11-06 02:12:31 +0530

    modified heroku push

--- vars/deployToHeroku.groovy
+++ vars/deployToHeroku.groovy
@@ -1,4 +1,4 @@
 def call(String herokuGitMaster) {
     checkout scm
-    sh "cd ${env.WORKSPACE} && git push https://git.heroku.com/${herokuGitMaster}.git"
+    sh "cd ${WORKSPACE} && git push https://git.heroku.com/${herokuGitMaster}.git"
 }
